a13ead7c2d489dd497b4c8b3599a499260da8d12
Hanamant Ramappa Kadlimatti <hanamantramap_k@photonint.com>
2018-09-19 20:26:55 +0530

    first commit

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,10 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('timerApp', ['ionic', 'angular-svg-round-progressbar'])
 
-.run(function($ionicPlatform) {
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs).
@@ -23,4 +24,77 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+})
+
+
+
+app.controller('timerCtrl', function($scope, $timeout){
+
+  $scope.myTimerFixed = 10;
+  $scope.myTimer = {}
+  $scope.myTimer.value = 10;
+
+  $scope.myTimer.startBtn = false;
+  $scope.myTimer.stopBtn = true;
+
+  var svg = document.getElementsByClassName('round-progress')[0]
+
+    svg.onload = function(){
+       $scope.radius = svg.getBoundingClientRect().width/2;
+    }
+
+  // $scope.radius = 100;
+
+  var myTimerVariable;
+
+  $scope.myCustomTimer = function(){
+     $scope.myTimer.value--;
+
+     if($scope.myTimer.value == 0){
+        $timeout.cancel(myTimerVariable);
+        complete(false)
+        return false; 
+     }
+
+     myTimerVariable = $timeout($scope.myCustomTimer, 1000)
+  }
+
+  $scope.start = function(){
+    console.log('started');
+    $scope.myTimer.startBtn = true;
+    $scope.myTimer.stopBtn = false;
+    myTimerVariable = $timeout($scope.myCustomTimer, 1000)
+  }
+
+  $scope.stop = function(){
+    console.log('Stopped');
+    //$scope.myTimer = 10
+    $timeout.cancel(myTimerVariable);
+    complete(true)
+  }
+
+  var complete = function(forcefullAbort){
+     if(forcefullAbort){
+       alert('You killed the HANAMANT timer');
+     }else{
+       alert('Timer completed')
+     }
+     $scope.myTimer.startBtn = false;
+     $scope.myTimer.stopBtn = true;
+  }
+
+  $scope.getStyle = function(){
+    var transform = 'translateY(606%) translateX(45%)';
+
+    return {
+        'top':  '50%',
+        'bottom': 'auto',
+        'left': '500%',
+        'transform': transform,
+        '-moz-transform': transform,
+        '-webkit-transform': transform,
+        'font-size': $scope.radius/3.5 + 'px'
+    };
+};
+
 })
